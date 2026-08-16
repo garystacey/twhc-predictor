806cc62977289952b5215f47be7938328dbb2c62
garystacey <garystacey@gmail.com>
2026-08-16 20:57:34 +0100

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -34,7 +34,11 @@ export default function LoginPage() {
   return (
     <main>
       <div className="container">
-        <div className="badge">TELFORD & WREKIN HC</div>
+<img
+  src="/TWHC-badge-white.png"
+  alt="Telford & Wrekin Hockey Club"
+  className="club-logo"
+/>
 
         <h1>THE PREDICTOR</h1>
 
