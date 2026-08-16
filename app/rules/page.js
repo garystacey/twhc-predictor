3ce9fb10804840e2a704c6dc1e29aef15e033b18
garystacey <garystacey@gmail.com>
2026-08-16 21:12:02 +0100

    Update page.js

--- app/rules/page.js
+++ app/rules/page.js
@@ -47,11 +47,11 @@ export default function RulesPage() {
   return (
     <main>
       <div className="container">
-<img
-  src="/TWHC-badge-white.png"
-  alt="Telford & Wrekin Hockey Club"
-  className="club-logo"
-/>
+        <img
+          src="/TWHC-badge-white.png"
+          alt="Telford & Wrekin Hockey Club"
+          className="club-logo"
+        />
 
         <h1>THE PREDICTOR</h1>
         <p className="subtitle">Competition Rules</p>
