f11a625c87154765a051a481acba33a1fad7509d
garystacey <garystacey@gmail.com>
2026-08-16 21:09:27 +0100

    Update page.js

--- app/reset-password/page.js
+++ app/reset-password/page.js
@@ -67,11 +67,11 @@ export default function ResetPasswordPage() {
   return (
     <main>
       <div className="container">
-<img
-  src="/TWHC-badge-white.png"
-  alt="Telford & Wrekin Hockey Club"
-  className="club-logo"
-/>
+        <img
+          src="/TWHC-badge-white.png"
+          alt="Telford & Wrekin Hockey Club"
+          className="club-logo"
+        />
 
         <h1>THE PREDICTOR</h1>
         <p className="subtitle">Reset Password</p>
