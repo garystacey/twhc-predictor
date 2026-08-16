ec29bd9f7af8ebffe4373c432d5746cae10e9275
garystacey <garystacey@gmail.com>
2026-08-16 21:07:55 +0100

    Update page.js

--- app/register/page.js
+++ app/register/page.js
@@ -75,11 +75,11 @@ export default function RegisterPage() {
   return (
     <main>
       <div className="container">
-<img
-  src="/TWHC-badge-white.png"
-  alt="Telford & Wrekin Hockey Club"
-  className="club-logo"
-/>
+        <img
+          src="/TWHC-badge-white.png"
+          alt="Telford & Wrekin Hockey Club"
+          className="club-logo"
+        />
 
         <h1>THE PREDICTOR</h1>
         <p className="subtitle">Create Your Account</p>
